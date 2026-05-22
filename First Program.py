09d9aadcc6d777f252a82672ec47361e0655a2ae
jds-codes <agsurjyo@gmail.com>
2026-05-22 20:50:04 +0600

    first commit

---
+++ First Program.py
@@ -0,0 +1,10 @@
+print ("Hello World")
+print (" This is my first program")
+print ("  I am learning Python")
+print ("   My name is Jyptirmoy Das Surjyo")
+print ("    I am 23 years old", "I am from Bangladesh")
+#suppose I need two numbers and i want to sum them
+num1 = 20
+num2 =50
+num3 =(num1+num2)
+print (      num3)
