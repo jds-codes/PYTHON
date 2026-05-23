64c811517c2454e1234605746d87128b67d4a542
jds-codes <agsurjyo@gmail.com>
2026-05-23 15:43:42 +0600

    updated

--- First Program.py
+++ First Program.py
@@ -38,3 +38,27 @@ elif myage <18:
     print ("You are underage")
 else:
     print ("You are not eligible to vote")
+
+red = "stop"
+yellow = "wait"
+green = "go"
+traffic = input("what is the traffic light colour?")
+if (traffic == "red"):
+    print ("stop")
+elif (traffic == "yellow"):
+    print ("wait")
+elif (traffic == "green"):
+    print ("go")
+else:
+    print ("light is broken")
+
+A = int(input("A : "))
+B = input("M/F : ")
+if ((A==1 or A==2) and G == "M"):
+    print("fee is 100")
+elif (A==3 or A==4 or G=="F"):
+    print ("fee is 200")
+elif (A==5 and G=="M"):
+    print("fee is 300")
+else:
+    print("no fee")
