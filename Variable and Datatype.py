744cbd0f06696e26b3ef5337e8d0ddbacc8c0ef9
jds-codes <agsurjyo@gmail.com>
2026-05-26 20:06:39 +0600

    updated

--- Variable and Datatype.py
+++ Variable and Datatype.py
@@ -4,7 +4,7 @@ user_name="Jyotirmoy Das Surjyo"
 is_logged_in=True
 print(user_name , user_age , product_price, is_logged_in)
 score=87
-score=score+5+5+99+9
+score=score+5+5+99
 print(score)
 age= int(input("age: "))
 vote = ("yes" ,"no") [age>=18]
